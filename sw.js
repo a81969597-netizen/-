60a5804a10800610d37f21be20253ef97db57680
a81969597-netizen <a81969597@gmail.com>
2026-05-30 20:05:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = 'cyber-strike-v2';
+const CACHE_NAME = 'cyber-strike-v3'; // غيرنا الرقم عشان المتصفح يحدّث الملفات
 const ASSETS = [
   './index.html',
   './hoho.png',    // الصورة القديمة
   './doksh.png',   // الصورة الجديدة للعدو
+  './ard.jpg',     // صورة الأرضية الجديدة
+  './het.jpg',     // صورة الحيطان الجديدة
   './shoot.mp3',
   './file.mp3',
   './ahoh.mp3',
   './elahwe.mp3',
-  './kosha.mp3'    // صوت القتل اللي لسه ضايفينه
+  './kosha.mp3'    // صوت القتل
 ];
 
 self.addEventListener('install', (e) => {
